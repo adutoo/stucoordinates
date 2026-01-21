88b22e9f6224166363cd3d73f646de611d6d9584
Aditya Singh <100421144+adutoo@users.noreply.github.com>
2026-01-21 18:35:24 +0530

    Create config.template.js

---
+++ config.template.js
@@ -0,0 +1,6 @@
+// config.template.js
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://nneyjeeswzqrpkkoxrdd.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5uZXlqZWVzd3pxcnBra294cmRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODQwNTUsImV4cCI6MjA4NDU2MDA1NX0.nqEp0x_3SkVsI1hV1ZJh79b25YPBpLgHAEMoKuI96hc",
+  GMAPS_KEY: "AIzaSyC5VODPbqv2q3QGfv9DAtW7VX8_gQHvWlo"
+};
